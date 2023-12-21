870e1ff76459dfccbe80ad982681a9cd6471bfa0
dmmontoto <dmontoto@ucsd.edu>
2023-12-21 12:14:38 -0800

    major improvements to 'what we do' page

--- src/pages/What-We-Do.jsx
+++ src/pages/What-We-Do.jsx
@@ -37,6 +37,54 @@ export default function WhatWeDo() {
           </div>
         </Col>
       </Row>
+
+      <Row className="Investing">
+        <Col xs={12} md={6}>
+          <div className="leftDiv">
+            <h1>Investing</h1>
+            <p>
+              {/* Your content here */}
+            </p>
+          </div>
+        </Col>
+        <Col xs={12} md={6} style={{ padding: '0' }}>
+          <div className="rightDiv">
+            <img src="./assets/InvestingImage.jpeg" alt="Investing" />
+          </div>
+        </Col>
+      </Row>
+
+      <Row className="RiskManagement">
+        <Col xs={12} md={6} style={{ padding: '0' }}>
+          <div className="leftDiv">
+            <img src="./assets/RiskManagementImage.jpg" alt="Risk Management" />
+          </div>
+        </Col>
+        <Col xs={12} md={6}>
+          <div className="rightDiv">
+            <h1>Risk Management</h1>
+            <p>
+              {/* Your content here */}
+            </p>
+          </div>
+        </Col>
+      </Row>
+
+      <Row className="HowItWorks">
+        <Col xs={12} md={6}>
+          <div className="leftDiv">
+            <h1>How It Works</h1>
+            <p>
+              {/* Your content here */}
+            </p>
+          </div>
+        </Col>
+        <Col xs={12} md={6} style={{ padding: '0' }}>
+          <div className="rightDiv">
+            <img src="./assets/HowItWorksImage.jpg" alt="How It Works" />
+          </div>
+        </Col>
+      </Row>
     </Container>
   );
 }
